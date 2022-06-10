02f658be5301953c518c2339ba91d00929ee4c12
louise <louise.silva@g.globo>
2022-06-10 16:39:01 -0300

    Resolvido

--- 0.strings/1.saudacao.js
+++ 0.strings/1.saudacao.js
@@ -2,3 +2,11 @@
 1. Crie 3 variaveis, nome, cidade e idade que irão receber valores definidos por você. Feito isso, crie uma string dizendo: Meu nome é ... tenho ... anos e moro na cidade ...
    Substituindo os ... pelos valores das respectivas variáveis. Feito isso, imprima o valor dessa variável na tela.
 */
+
+var nome = 'Louise'
+var cidade = 'Nilopolis'
+var idade = 18
+
+var string = 'Meu nome é ' + nome + ' tenho ' + idade + ' anos e moro na cidade de ' + cidade
+
+console.log(string)
