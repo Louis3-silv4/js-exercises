0b0e7811af7884418517cb3f6acfe56dd9742822
louise <louise.silva@g.globo>
2022-06-21 10:17:38 -0300

    criando objeto com const

--- 0.strings/1.saudacao.js
+++ 0.strings/1.saudacao.js
@@ -3,10 +3,10 @@
    Substituindo os ... pelos valores das respectivas variáveis. Feito isso, imprima o valor dessa variável na tela.
 */
 
-var nome = 'Louise'
-var cidade = 'Nilopolis'
-var idade = 18
+const nome = 'Louise'
+const cidade = 'Nilopolis'
+const idade = 18
 
-var string = 'Meu nome é ' + nome + ' tenho ' + idade + ' anos e moro na cidade de ' + cidade
+const string = `Meu nome é ${nome} tenho ${idade} anos e moro na cidade de ${cidade}`
 
 console.log(string)
