062322fafff768623e1c33aca7fe66a00be85561
louise <louise.silva@g.globo>
2022-06-21 10:25:36 -0300

    usando spread para concatenar

--- 1.arrays/3.concat.js
+++ 1.arrays/3.concat.js
@@ -4,9 +4,12 @@
    Ex: primeiro = [1, 2, 3] segundo = [1, 2, 3, 11, 22, 33], terceiro precisa ser [1, 2, 3, 1, 2, 3, 11, 22, 33]
 */
 
-var primeiroArray = ['Azul', 'Fusca']
-var segundoArray = ['Verde', 'Gol']
+const primeiroArray = ['Azul', 'Fusca']
+const segundoArray = ['Verde', 'Gol']
 
-var terceiroArray = [ primeiroArray.concat(segundoArray)]
+const terceiroArray = [ primeiroArray.concat(segundoArray)]
 
-console.log(terceiroArray)
+const quartoArray = [...primeiroArray, ...segundoArray]
+
+console.log(quartoArray, terceiroArray)
+//TODO: Usar spread operator  
